259a549a8fa7c8880b17d81f3a66dc2e0bf7bda9
YassineJanfaoui <janfaouimohamed16@gmail.com>
2026-04-20 21:42:35 +0100

    greatly improved animation

---
+++ alia_django/static/css/avatar-animation.js
@@ -0,0 +1,368 @@
+export const avatarKeyframes = [
+  {
+    "time": 0,
+    "bones": {
+      "Head": {
+        "x": -0.11116471467994962,
+        "y": -0.15867356072887664,
+        "z": 0
+      },
+      "LeftShoulder": {
+        "x": 1.6935678427093266,
+        "y": -0.2528024135126426,
+        "z": -1.5690309064252106
+      },
+      "LeftArm": {
+        "x": 1.1698389812886327,
+        "y": 0.16019027263660118,
+        "z": -0.29802802684077756
+      },
+      "LeftForeArm": {
+        "x": 0.05000000000000001,
+        "y": 0.06,
+        "z": 1.1727292082667549
+      },
+      "RightShoulder": {
+        "x": 1.739502077248299,
+        "y": 0.27897958722917554,
+        "z": 1.571556639153765
+      },
+      "RightArm": {
+        "x": 1.12,
+        "y": 0.09,
+        "z": 0.12
+      },
+      "RightForeArm": {
+        "x": 0.03,
+        "y": 0.01,
+        "z": -1.973668047456528
+      }
+    },
+    "morphs": {
+      "jawOpen": 0,
+      "mouthOpen": 0,
+      "viseme_sil": 0,
+      "viseme_PP": 0,
+      "viseme_FF": 0,
+      "viseme_TH": 0,
+      "viseme_DD": 0,
+      "viseme_kk": 0,
+      "viseme_CH": 0,
+      "viseme_SS": 0,
+      "viseme_nn": 0,
+      "viseme_RR": 0,
+      "viseme_aa": 0,
+      "viseme_E": 0,
+      "viseme_I": 0,
+      "viseme_O": 0,
+      "viseme_U": 0,
+      "eyeBlinkLeft": 0,
+      "eyeBlinkRight": 0
+    }
+  },
+  {
+    "time": 1,
+    "bones": {
+      "Head": {
+        "x": -0.11,
+        "y": -0.31,
+        "z": 0
+      },
+      "LeftShoulder": {
+        "x": 1.6935678427093266,
+        "y": -0.2528024135126426,
+        "z": -1.5690309064252106
+      },
+      "LeftArm": {
+        "x": 1.1698389812886327,
+        "y": 0.16019027263660118,
+        "z": -0.29802802684077756
+      },
+      "LeftForeArm": {
+        "x": 0.05,
+        "y": -0.14,
+        "z": 1.76
+      },
+      "RightShoulder": {
+        "x": 1.739502077248299,
+        "y": 0.27897958722917554,
+        "z": 1.571556639153765
+      },
+      "RightArm": {
+        "x": 1.12,
+        "y": 0.09,
+        "z": 0.12
+      },
+      "RightForeArm": {
+        "x": 0.03,
+        "y": 0.01,
+        "z": -1.973668047456528
+      }
+    },
+    "morphs": {
+      "jawOpen": 0,
+      "mouthOpen": 0,
+      "viseme_sil": 0,
+      "viseme_PP": 0,
+      "viseme_FF": 0,
+      "viseme_TH": 0,
+      "viseme_DD": 0,
+      "viseme_kk": 0,
+      "viseme_CH": 0,
+      "viseme_SS": 0,
+      "viseme_nn": 0,
+      "viseme_RR": 0,
+      "viseme_aa": 0,
+      "viseme_E": 0,
+      "viseme_I": 0,
+      "viseme_O": 0,
+      "viseme_U": 0,
+      "eyeBlinkLeft": 0,
+      "eyeBlinkRight": 0
+    }
+  },
+  {
+    "time": 2,
+    "bones": {
+      "Head": {
+        "x": -0.14,
+        "y": -0.04,
+        "z": 0
+      },
+      "LeftShoulder": {
+        "x": 1.6935678427093266,
+        "y": -0.2528024135126426,
+        "z": -1.5690309064252106
+      },
+      "LeftArm": {
+        "x": 1.1698389812886327,
+        "y": 0.16019027263660118,
+        "z": -0.29802802684077756
+      },
+      "LeftForeArm": {
+        "x": 0.05,
+        "y": -0.0435989158167977,
+        "z": 1.4769322948389378
+      },
+      "RightShoulder": {
+        "x": 1.739502077248299,
+        "y": 0.27897958722917554,
+        "z": 1.571556639153765
+      },
+      "RightArm": {
+        "x": 1.12,
+        "y": 0.39,
+        "z": -0.37
+      },
+      "RightForeArm": {
+        "x": 0.03,
+        "y": 0.01,
+        "z": -1.87
+      }
+    },
+    "morphs": {
+      "jawOpen": 0,
+      "mouthOpen": 0,
+      "viseme_sil": 0,
+      "viseme_PP": 0,
+      "viseme_FF": 0,
+      "viseme_TH": 0,
+      "viseme_DD": 0,
+      "viseme_kk": 0,
+      "viseme_CH": 0,
+      "viseme_SS": 0,
+      "viseme_nn": 0,
+      "viseme_RR": 0,
+      "viseme_aa": 0,
+      "viseme_E": 0,
+      "viseme_I": 0,
+      "viseme_O": 0,
+      "viseme_U": 0,
+      "eyeBlinkLeft": 0,
+      "eyeBlinkRight": 0
+    }
+  },
+  {
+    "time": 3,
+    "bones": {
+      "Head": {
+        "x": -0.14,
+        "y": -0.04,
+        "z": 0
+      },
+      "LeftShoulder": {
+        "x": 1.59,
+        "y": -0.14,
+        "z": -1.57
+      },
+      "LeftArm": {
+        "x": 1.17,
+        "y": -0.01,
+        "z": 0.76
+      },
+      "LeftForeArm": {
+        "x": 0.05,
+        "y": -0.47,
+        "z": 1.48
+      },
+      "RightShoulder": {
+        "x": 1.739502077248299,
+        "y": 0.27897958722917554,
+        "z": 1.571556639153765
+      },
+      "RightArm": {
+        "x": 0.99,
+        "y": 0.16,
+        "z": -0.34
+      },
+      "RightForeArm": {
+        "x": 0.03,
+        "y": 0.06,
+        "z": -0.74
+      }
+    },
+    "morphs": {
+      "jawOpen": 0,
+      "mouthOpen": 0,
+      "viseme_sil": 0,
+      "viseme_PP": 0,
+      "viseme_FF": 0,
+      "viseme_TH": 0,
+      "viseme_DD": 0,
+      "viseme_kk": 0,
+      "viseme_CH": 0,
+      "viseme_SS": 0,
+      "viseme_nn": 0,
+      "viseme_RR": 0,
+      "viseme_aa": 0,
+      "viseme_E": 0,
+      "viseme_I": 0,
+      "viseme_O": 0,
+      "viseme_U": 0,
+      "eyeBlinkLeft": 0,
+      "eyeBlinkRight": 0
+    }
+  },
+  {
+    "time": 4,
+    "bones": {
+      "Head": {
+        "x": -0.14,
+        "y": -0.27,
+        "z": 0
+      },
+      "LeftShoulder": {
+        "x": 1.59,
+        "y": -0.14,
+        "z": -1.57
+      },
+      "LeftArm": {
+        "x": 1.17,
+        "y": -0.01,
+        "z": 0.76
+      },
+      "LeftForeArm": {
+        "x": 0.05,
+        "y": -0.07,
+        "z": -0.34
+      },
+      "RightShoulder": {
+        "x": 1.739502077248299,
+        "y": 0.27897958722917554,
+        "z": 1.571556639153765
+      },
+      "RightArm": {
+        "x": 0.99,
+        "y": -0.07,
+        "z": -0.34
+      },
+      "RightForeArm": {
+        "x": 0.03,
+        "y": 0.06,
+        "z": -1.71
+      }
+    },
+    "morphs": {
+      "jawOpen": 0,
+      "mouthOpen": 0,
+      "viseme_sil": 0,
+      "viseme_PP": 0,
+      "viseme_FF": 0,
+      "viseme_TH": 0,
+      "viseme_DD": 0,
+      "viseme_kk": 0,
+      "viseme_CH": 0,
+      "viseme_SS": 0,
+      "viseme_nn": 0,
+      "viseme_RR": 0,
+      "viseme_aa": 0,
+      "viseme_E": 0,
+      "viseme_I": 0,
+      "viseme_O": 0,
+      "viseme_U": 0,
+      "eyeBlinkLeft": 0,
+      "eyeBlinkRight": 0
+    }
+  },
+  {
+    "time": 5,
+    "bones": {
+      "Head": {
+        "x": -0.11116471467994962,
+        "y": -0.15867356072887664,
+        "z": 0
+      },
+      "LeftShoulder": {
+        "x": 1.6935678427093266,
+        "y": -0.2528024135126426,
+        "z": -1.5690309064252106
+      },
+      "LeftArm": {
+        "x": 1.1698389812886327,
+        "y": 0.16019027263660118,
+        "z": -0.29802802684077756
+      },
+      "LeftForeArm": {
+        "x": 0.05000000000000001,
+        "y": 0.06,
+        "z": 1.1727292082667549
+      },
+      "RightShoulder": {
+        "x": 1.739502077248299,
+        "y": 0.27897958722917554,
+        "z": 1.571556639153765
+      },
+      "RightArm": {
+        "x": 1.12,
+        "y": 0.09,
+        "z": 0.12
+      },
+      "RightForeArm": {
+        "x": 0.03,
+        "y": 0.01,
+        "z": -1.973668047456528
+      }
+    },
+    "morphs": {
+      "jawOpen": 0,
+      "mouthOpen": 0,
+      "viseme_sil": 0,
+      "viseme_PP": 0,
+      "viseme_FF": 0,
+      "viseme_TH": 0,
+      "viseme_DD": 0,
+      "viseme_kk": 0,
+      "viseme_CH": 0,
+      "viseme_SS": 0,
+      "viseme_nn": 0,
+      "viseme_RR": 0,
+      "viseme_aa": 0,
+      "viseme_E": 0,
+      "viseme_I": 0,
+      "viseme_O": 0,
+      "viseme_U": 0,
+      "eyeBlinkLeft": 0,
+      "eyeBlinkRight": 0
+    }
+  }
+];
